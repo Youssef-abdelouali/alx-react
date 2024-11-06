cb76c263a14a580d0782e0d3fe7db00c2324474d
Youssef-Abdelouali <youssefabdelouali225@gmail.com>
2024-11-06 20:02:17 +0000

    tasks done

--- 0x01-react_intro/task_0/dashboard/src/App.js
+++ 0x01-react_intro/task_0/dashboard/src/App.js
@@ -1,21 +1,20 @@
+import logo from './holberton-logo.jpeg';
 import './App.css';
-import holberton_logo from './holberton-logo.jpg';
 
-function App() {
-  return (
-    <>
+function App(){
+	return(
+    <div className="App">
       <div className="App-header">
-        <img src={holberton_logo} alt="logo" />
+        <img src={logo} className="App-logo" alt="logo" />
         <h1>School dashboard</h1>
       </div>
       <div className="App-body">
         <p>Login to access the full dashboard</p>
       </div>
       <div className="App-footer">
-        <p>Copyright 2020 - Holberton School</p>
+        Copyright {getFullYear()} - {getFooterCopy()}
       </div>
-    </>
+    </div>
   );
 }
-
 export default App;
